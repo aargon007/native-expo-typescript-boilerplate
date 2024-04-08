f057ccd87a4ba0ce54de0e89493250b6de39d650
aargon007 <muhai4959@gmail.com>
2024-04-08 22:03:17 +0600

    added utils and component

---
+++ component/Home/Slider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Slider = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default Slider;
