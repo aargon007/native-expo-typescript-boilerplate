144ea3fce1ced774c3182c6a230bf37d28030bb6
aargon007 <muhai4959@gmail.com>
2024-06-06 09:30:58 +0600

    updated package

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,8 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import { useThemeScheme } from '@/constants/useThemeScheme';
 import { type StackNavigation } from '@/navigators/RootNavigator';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { Path, Svg } from 'react-native-svg';
+import { Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const Home = () => {
@@ -56,31 +55,5 @@ const styles = EStyleSheet.create({
         borderColor: '$borderColorPrimary',
     },
 });
-
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         flexDirection: "column",
-//         justifyContent: "space-between",
-//         alignItems: "center",
-//         paddingVertical: 50,
-//         paddingHorizontal: 30,
-//     },
-//     buttonStyle: {
-//         height: 45,
-//         width: "100%",
-//         margin: 12,
-//         borderWidth: 1,
-//         padding: 10,
-//         backgroundColor: "#004944",
-//         borderRadius: 10,
-//     },
-//     btnText: {
-//         color: "#fff",
-//         textAlign: "center",
-//         fontSize: 16,
-//     },
-// })
 
 export default Home;
